5e5958ba58381f31bb0459e12139de46bbe20518
Alex Rael <alprael22@gmail.com>
2019-07-23 22:48:35 -0600

    added max and min values of byte, short, int, and long

--- src/com/alprael/ByteShortValues.java
+++ src/com/alprael/ByteShortValues.java
@@ -4,6 +4,26 @@ public class ByteShortValues {
 
   public static void main(String[] args) {
 
+    byte myMaxByte = Byte.MAX_VALUE;
+    byte myMinByte = Byte.MIN_VALUE;
+    System.out.println("Byte max value= " + myMaxByte);
+    System.out.println("Byte min value= " + myMinByte);
+
+    short myMaxShort = Short.MAX_VALUE;
+    short myMinShort = Short.MIN_VALUE;
+    System.out.println("Short max value= " + myMaxShort);
+    System.out.println("Short min value= " + myMinShort);
+
+    int myMaxInt = Integer.MAX_VALUE;
+    int myMinInt = Integer.MIN_VALUE;
+    System.out.println("Integer max value= " + myMaxInt);
+    System.out.println("Integer min value= " + myMinInt);
+
+    long myMaxLong = Long.MAX_VALUE;
+    long myMinLong = Long.MIN_VALUE;
+    System.out.println("Long max value= " + myMaxLong);
+    System.out.println("Long min value= " + myMinLong);
+
     byte myByte = 127;
     short myShort = 32_000;
     int myInt = 100_000;
@@ -15,6 +35,12 @@ public class ByteShortValues {
     short shortTotal = (short) (50_000 + 10 * (myByte + myShort + myInt));
 
     System.out.println(shortTotal);
+
+    float myMaxFloat = Float.MAX_VALUE;
+    float myMinFloat = Float.MIN_VALUE;
+
+    System.out.println("Float max value= " + myMaxFloat);
+    System.out.println("Float min value= " + myMinFloat);
 
   }
 
